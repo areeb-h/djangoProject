3cc336cc17e999a605bcf2515f7a7b044598eaf9
areeb-h <areeb.hw@gmail.com>
2021-10-11 11:38:10 +0500

    new

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
   
   import React from 'react';
   import ReactDOM from 'react-dom';
-  import 'bootstrap/dist/css/bootstrap.min.css';       // add this
+  import './bootstrap/css/bootstrap.min.css';       // add this
   import './index.css';
   import App from './App';
   import * as serviceWorker from './serviceWorker';
